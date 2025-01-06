02f678623fadc854dfe5825a961667e6746f9c85
Michelle Nygård <mican17@student.sdu.dk>
2025-01-06 15:30:29 +0100

    Fixed database

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -9,11 +9,11 @@ class GameController extends Controller
 {
     public function index() {
         $games = Game::all();
-        return view('game.index', compact('games'));
+        return view('games.index', compact('games'));
     }
 
     public function create() {
-        return view('game.create');
+        return view('games.create');
     }
 
     public function store(Request $request) {
@@ -25,15 +25,15 @@ class GameController extends Controller
         ]);
 
         Game::create($request->all());
-        return redirect()->route('games.index')->with('success', 'Game added successfully.');
+        return redirect()->route('games.create')->with('success', 'Game added successfully.');
     }
 
     public function show(Game $game) {
-        return view('game.show', compact('game'));
+        return view('games.show', compact('game'));
     }
 
     public function edit(Game $game) {
-        return view('game.edit', compact('game'));
+        return view('games.edit', compact('game'));
     }
 
     public function update(Request $request, Game $game) {
--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+
+    // Add the _token field to the fillable property
+    protected $fillable = [
+        'title',
+        'developer',
+        'genre',
+        'release_year'
+    ];
 }
